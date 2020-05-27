4cbdaf0ac10d9b548981244b93e6611927b03d64
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-05-27 23:50:27 +0900

    add experiment and experiment status model

--- server/igloo/models.py
+++ server/igloo/models.py
@@ -1,21 +1,36 @@
 from django.db import models
 
 
+class ExperimentStatus(models.Model):
+    """Experiment Status List (e.g. Waiting, Preparing, Running, Stopped etc)
+    """
+    status = models.CharField(max_length=15)
+
+
+# class Category(models.Model):
+#     pass
+
+
+# class Schedule(models.Model):
+#     pass
+
+
 class Experiment(models.Model):
-    pass
+    """A/B Test Experiment idea
+    """
+    code = models.CharField(
+        max_length=20, help_text='Enter Experiment code(e.g. EX123b)', unique=True)
+    title = models.CharField(
+        max_length=200, help_text='Enter Experiment full title(e.g. Full funnel test)')
+    description = models.TextField(
+        max_length=1000, help_text='Enter Experiment short description', null=True)
+    status = models.ForeignKey(
+        ExperimentStatus, on_delete=models.SET_NULL, null=True)
+    impact = models.IntegerField(null=True)
+    confidence = models.IntegerField(null=True)
+    ease = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
-
-class Status(models.Model):
-    pass
-
-
-class ICEScore(models.Model):
-    pass
-
-
-class Category(models.Model):
-    pass
-
-
-class ExperimentSchedule(models.Model):
-    pass
+    class Meta:
+        ordering = ['created_at', 'updated_at', 'impact', 'confidence', 'ease']
