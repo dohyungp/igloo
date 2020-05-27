9ab2d7ce6746a81e6d196bd75d460c402672d193
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-05-27 22:53:51 +0900

    add model skeleton

--- server/igloo/models.py
+++ server/igloo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Experiment(models.Model):
+    pass
+
+
+class Status(models.Model):
+    pass
+
+
+class ICEScore(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class ExperimentSchedule(models.Model):
+    pass
