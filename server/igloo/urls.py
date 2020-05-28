b1b3e4acae280d72dcf0c7734415c86c4c78ee93
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-05-28 21:31:09 +0900

    Change view as class

--- server/igloo/urls.py
+++ server/igloo/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from igloo import views
 
 urlpatterns = [
-    path('experiments/', views.experiment_list),
+    path('experiments/', views.ExperimentList.as_view()),
+    path('experiments/<int:pk>/', views.ExperimentDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/igloo/views.py
+++ server/igloo/views.py
@@ -1,23 +1,60 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.http import Http404
+
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import filters
+
+
 from igloo.models import Experiment
 from igloo.serializers import ExperimentSerializer
 
 
-@csrf_exempt
-def experiment_list(request):
-    """List all experiments, or create a new experiment.
-    """
-    if request.method == 'GET':
-        experiments = Experiment.objects.all()
-        serializer = ExperimentSerializer(experiments, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ExperimentSerializer(data=data)
+class ExperimentList(generics.ListCreateAPIView):
+    queryset = Experiment.objects.all()
+    serializer_class = ExperimentSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['impact', 'confidence',
+                       'ease', 'created_at', 'updated_at']
+
+    def list(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = ExperimentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class ExperimentDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Experiment.objects.get(pk=pk)
+        except Experiment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        experiment = self.get_object(pk)
+        serializer = ExperimentSerializer(experiment)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        experiment = self.get_object(pk)
+        serializer = ExperimentSerializer(experiment, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    return HttpResponse(status=405)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        experiment = self.get_object(pk)
+        serializer = ExperimentSerializer(
+            experiment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        experiment = self.get_object(pk)
+        experiment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
