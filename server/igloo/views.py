ea759be93743464fc80e8f71971d3619245673fb
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-05-28 16:07:09 +0900

    add experiment list view

--- server/igloo/views.py
+++ server/igloo/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from igloo.models import Experiment
+from igloo.serializers import ExperimentSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def experiment_list(request):
+    """List all experiments, or create a new experiment.
+    """
+    if request.method == 'GET':
+        experiments = Experiment.objects.all()
+        serializer = ExperimentSerializer(experiments, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ExperimentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    return HttpResponse(status=405)
