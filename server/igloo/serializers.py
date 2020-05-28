55a9812ddff3c2ce097e2771b6b70dccb476f2e6
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-05-28 14:51:26 +0900

    fill out ExperimentSerializer

--- server/igloo/serializers.py
+++ server/igloo/serializers.py
@@ -2,5 +2,10 @@ from rest_framework import serializers
 from igloo.models import Experiment
 
 
-class ExperimentSerializer(serializers.Serializer):
-    pass
+class ExperimentSerializer(serializers.ModelSerializer):
+    status = serializers.StringRelatedField()
+
+    class Meta:
+        model = Experiment
+        fields = ['id', 'code', 'title', 'description', 'status',
+                  'impact', 'confidence', 'ease', 'created_at', 'updated_at']
