0141919546505e0257bb646808a1f023152de946
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-05-29 23:06:24 +0900

    add status serializer for pk represntaion

--- server/igloo/serializers.py
+++ server/igloo/serializers.py
@@ -1,11 +1,21 @@
+from django.core.exceptions import ValidationError, EmptyResultSet
 from rest_framework import serializers
-from igloo.models import Experiment
+from igloo.models import Experiment, ExperimentStatus
+
+
+class ExperimentStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExperimentStatus
+        fields = ['id', 'status']
 
 
 class ExperimentSerializer(serializers.ModelSerializer):
-    status = serializers.StringRelatedField()
-
     class Meta:
         model = Experiment
         fields = ['id', 'code', 'title', 'description', 'status',
                   'impact', 'confidence', 'ease', 'created_at', 'updated_at']
+
+    def to_representation(self, obj):
+        if self.fields.get('status'):
+            self.fields['status'] = ExperimentStatusSerializer()
+        return super().to_representation(obj)
