d50e59c365a3361237baf1fb53fbbb56c0e4c4a5
Dohyung Park <dohyung.prk@gmail.com>
2020-05-31 15:24:47 +0900

    remove unused import

--- server/igloo/views.py
+++ server/igloo/views.py
@@ -1,9 +1,4 @@
-from django.http import Http404
-
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import filters
 
 from django_filters.rest_framework import DjangoFilterBackend
