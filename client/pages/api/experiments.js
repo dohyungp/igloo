b6ed3871c1beeea9296bf19eea22e6ed89a60b4e
Dohyung Park <dohyung.prk@gmail.com>
2020-06-04 23:59:26 +0900

    Change next proxy logic more simplify

--- client/pages/api/experiments.js
+++ client/pages/api/experiments.js
@@ -2,31 +2,16 @@ import axios from "axios";
 
 const API_URL = "http://server:8000";
 
-const isEmptyObject = (obj) => {
-  return Object.keys(obj).length === 0 && obj.constructor === Object;
-};
-
-async function getExperiments(query) {
-  let requestURL = `${API_URL}/experiments`;
-  if (!isEmptyObject(query)) {
-    let queryString = new URLSearchParams(query).toString();
-    requestURL = `${API_URL}/experiments/?${queryString}`;
-  }
-
-  const response = await axios.get(requestURL);
-  const data = response.data;
-  return data;
-}
-
 export default async (req, res) => {
-  let data = {};
-  switch (req.method) {
-    case "GET":
-      data = await getExperiments(req.query);
-      res.json(data);
-      break;
-    default:
-      res.setHeader("Allow", ["GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
+  console.log(req);
+  const config = {
+    url: `${API_URL}/experiments/`,
+    method: req.method,
+    params: req.query,
+    data: req.body,
+    headers: req.headers,
+  };
+  const response = await axios(config);
+
+  res.json(response.data);
 };
