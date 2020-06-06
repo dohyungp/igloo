a7e67dd1e0d0671593d90f53f155c45501088f95
Dohyung Park <dohyung.prk@gmail.com>
2020-06-06 11:53:44 +0900

    Add Layout component to MainLayout

--- client/components/MainLayout.js
+++ client/components/MainLayout.js
@@ -7,16 +7,18 @@ import styles from "../styles/mainlayout.module.css";
 
 export const MainLayout = ({ title, children }) => {
   return (
-    <Layout>
+    <>
       <Head>
         <title>IGLOO | {title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <header className={styles.header}>
-        <Header />
-      </header>
-      <main className={styles.main}>{children}</main>
-    </Layout>
+      <Layout>
+        <header className={styles.header}>
+          <Header />
+        </header>
+        <main className={styles.main}>{children}</main>
+      </Layout>
+    </>
   );
 };
