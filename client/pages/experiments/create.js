7c15bfbd74b4b294de09372b11d5be3a8a7143c3
Dohyung Park <dohyung.prk@gmail.com>
2020-06-07 02:07:22 +0900

    Add experiment create page

--- client/pages/experiments/create.js
+++ client/pages/experiments/create.js
@@ -1,102 +1,91 @@
 import { useState } from "react";
-import Link from "next/link";
+import Router from "next/router";
+
 import useRequest from "../../libs/useRequest";
 import fetch from "../../libs/fetch";
+import { MainLayout } from "../../components/MainLayout";
+
+import { Form, Input, Select, Tag, InputNumber, Button, Tooltip } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 14 },
+};
 
 const ExperimentCreatePage = () => {
-  const [formData, updateFormData] = useState({});
-  const handleChange = (e) => {
-    updateFormData({
-      ...formData,
-      [e.target.name]: e.target.value.trim(),
-    });
-  };
-  const { data: experimentStatus } = useRequest({
+  const { data: status, error: statusError } = useRequest({
     url: "/api/status",
   });
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const onFinish = async (values) => {
     const { data } = await fetch({
       method: "POST",
       url: "/api/experiments",
-      data: formData,
+      data: values,
     });
+    Router.push("/");
   };
+
   return (
-    <div>
-      <Link href="/">
-        <a>홈으로</a>
-      </Link>
-      <br />
-      <div>
-        <form id="experiment-create-form" onSubmit={handleSubmit}>
-          <div>
-            <input
-              type="text"
-              name="code"
-              placeholder="code"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="title"
-              placeholder="title"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <select name="status">
-              {experimentStatus
-                ? experimentStatus?.results?.map((status) => {
-                    return (
-                      <option value={status.id} key={status.id}>
-                        {status.name}
-                      </option>
-                    );
-                  })
-                : ""}
-            </select>
-          </div>
-          <div>
-            <input
-              type="number"
-              min="0"
-              max="10"
-              name="impact"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              min="0"
-              max="10"
-              name="confidence"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              min="0"
-              max="10"
-              name="ease"
-              onChange={handleChange}
-            />
-          </div>
-        </form>
-        <textarea
-          form="experiment-create-form"
-          name="description"
-          onChange={handleChange}
-        />
-        <button type="submit" onClick={handleSubmit}>
-          Submit
-        </button>
-      </div>
-    </div>
+    <MainLayout title="Create a idea">
+      {statusError ? message.error("Fetching status data was failed") : ""}
+      <Form {...layout} initialValues={{ size: "large" }} onFinish={onFinish}>
+        <Form.Item
+          name="code"
+          label={
+            <span>
+              Code&nbsp;
+              <Tooltip title="어떤 실험인지 확인할 수 있도록 코드를 입력해주세요">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          }
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="status" label="Status">
+          <Select loading={!(status || statusError)} allowClear>
+            <Select.OptGroup label="Status">
+              {status?.results?.map((option) => {
+                return (
+                  <Select.Option value={option.id} key={option.id}>
+                    <Tag
+                      color={option?.tag_color ? option.tag_color : "default"}
+                    >
+                      {option.name}
+                    </Tag>
+                  </Select.Option>
+                );
+              })}
+            </Select.OptGroup>
+            <Select.Option>
+              <div onClick={(e) => console.log(e)}>Create a new status</div>
+            </Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <Input.TextArea autoSize={{ minRows: 8 }} allowClear />
+        </Form.Item>
+        <Form.Item name="impact" label="Impact">
+          <InputNumber min={0} max={10} />
+        </Form.Item>
+        <Form.Item name="confidence" label="Confidence">
+          <InputNumber min={0} max={10} />
+        </Form.Item>
+        <Form.Item name="ease" label="Ease">
+          <InputNumber min={0} max={10} />
+        </Form.Item>
+        <Form.Item wrapperCol={{ md: { span: 12, offset: 4 } }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </MainLayout>
   );
 };
 
